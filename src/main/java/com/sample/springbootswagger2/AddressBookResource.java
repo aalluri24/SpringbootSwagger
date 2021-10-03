ecfd6f4d9b01ffcb3beefdcdb2b45a332e120b50
Alluri, Aravind <Aravind.Alluri@meijer.com>
2021-10-02 20:34:51 -0400

    Added more swagger documentation
    Added more swagger documentation using different custom annotations using Docket etc.,

--- src/main/java/com/sample/springbootswagger2/AddressBookResource.java
+++ src/main/java/com/sample/springbootswagger2/AddressBookResource.java
@@ -1,5 +1,7 @@
 package com.sample.springbootswagger2;
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -13,17 +15,26 @@ public class AddressBookResource {
     ConcurrentHashMap<String,Contact> contacts = new ConcurrentHashMap();
 
     @PostMapping("/addContact")
+    @ApiOperation(value = "Add contact to an address book",
+        notes ="Pass a JSON body with Contact Info to add to the address book")
     public void addContact(@RequestBody Contact contact) {
         contacts.put(contact.getId(), contact);
     }
 
     @GetMapping("/getAllContacts")
+    @ApiOperation(value = "Finds all contacts",
+            notes = "Returns all contacts from the address book",
+            response = Contact.class)
     public List<Contact> getAllContacts() {
         return new ArrayList<Contact>(contacts.values());
     }
 
     @GetMapping("/getContact/{id}")
-    public Contact getContact(@PathVariable String id) {
+    @ApiOperation(value = "Finds contacts by id",
+        notes = "Provide an id to look-up specific contact from the address book",
+        response = Contact.class)
+    public Contact getContact(@ApiParam(value = "ID value for the contact you need to retrieve", required = true)
+            @PathVariable String id) {
         return contacts.get(id);
     }
 }
--- src/main/java/com/sample/springbootswagger2/Contact.java
+++ src/main/java/com/sample/springbootswagger2/Contact.java
@@ -1,11 +1,17 @@
 package com.sample.springbootswagger2;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
+@ApiModel(description = "Details about the contact")
 public class Contact {
 
+    @ApiModelProperty(notes= "The unique id of the contact")
     private String id;
+    @ApiModelProperty(notes = "The person's name")
     private String name;
+    @ApiModelProperty(value = "The person's contact number")
     private String phoneNum;
 
     public Contact(String id, String name, String phoneNum) {
